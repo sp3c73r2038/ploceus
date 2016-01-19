cd9d330c760592b82bb095bfb6699f7bf530c7bb
nobody <nobody@nowhere.org>
2016-01-19 18:06:58 +0800

    add tools.files.{group,mode,umask,chown,chmod}

--- ploceus/tools/files.py
+++ ploceus/tools/files.py
@@ -24,12 +24,45 @@ def owner(path, use_sudo=None, sudo_user=None):
     return out.decode(env.encoding)
 
 
-def upload_file(src, dest, owner=None, group=None, mode=None):
+def group(path, use_sudo=None, sudo_user=None):
+    _ = (use_sudo and sudo) or run
+    rv = _('stat -c %%U %s' % path, quiet=True).stdout.read().strip()
+    return out.decode(env.encoding)
+
+
+def mode(path, use_sudo=None, sudo_user=None):
+    _ = (use_sudo and sudo) or run
+    rv = _('stat -c %%a %s' % path, quiet=True).stdout.read().strip()
+    return '0' + out.decode(env.encoding)
+
+
+def umask(path, use_sudo=None, sudo_user=None):
+    _ = (use_sudo and sudo) or run
+    rv = _('umask', quiet=True).stdout.read().strip()
+    return rv.decode(env.encoding)
+
+
+def chown(path, user, grp, use_sudo=None, sudo_user=None):
+    _ = (use_sudo and sudo) or run
+    return _('chown %s:%s %s' % (user, grp, path))
+
+
+def chmod(path, mode, use_sudo=None, sudo_user=None):
+    _ = (use_sudo and sudo) or run
+    return _('chmod %s %s' % (mode, path))
+
+
+def upload_file(src, dest, user=None, grp=None, mode=None):
     ctx = context_manager.get_context()
     ssh = ctx['sshclient']
     ssh.put_file(src, dest)
 
-    # TODO: checks
+    if (user and (owner(dest) != user)) or (grp and (group(dest) != grp)):
+        chown(dest, user, grp)
+
+    if mode and (mode(path) != mode):
+        chmod(dest, mode)
+
 
 
 def upload_template(src, dest, jinja_ctx=None,
