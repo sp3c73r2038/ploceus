f7f0b2c2b77ea9f5a52ce6dc92c80b7beff56522
80x24 <80x24@momoka.net>
2016-01-21 17:18:15 +0800

    add task running api

--- ploceus/api.py
+++ ploceus/api.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
-from .decorator import task
-from .helper import run, sudo
+from ploceus.decorator import task
+from ploceus.helper import run, sudo
 
 
 def run_task_by_group(group_name, task, extra_vars=None):
-    pass
+    from ploceus import g
+    g.inventory.find_inventory()
+    group = g.inventory.get_target_hosts(group_name)
+    hosts = group['hosts']
+    extra_vars = extra_vars or {}
+    if 'vars' in group:
+        extra_vars.update(group['vars'])
+    for host in hosts:
+        task.run(host, extra_vars)
