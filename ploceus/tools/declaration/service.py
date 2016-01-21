2ee74a223de84570c781b0296d218f6b5e078734
nobody <nobody@nowhere.org>
2016-01-21 18:09:53 +0800

    add service

---
+++ ploceus/tools/declaration/service.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from ploceus.tools.service import start, stop, restart, reload
+from ploceus.logger import log
+
+
+def started(service):
+    if not is_running(service):
+        start(service)
+    log('%s started' % service, prefix='service')
+
+
+def stopped(service):
+    if is_running(service):
+        stop(service)
+    log('%s stopped' % service, prefix='service')
+
+
+def restarted(service):
+    if is_running(service):
+        restart(service)
+    else:
+        start(service)
+    log('%s restarted' % service, prefix='service')
+
+
+def reloaded(service):
+    reload(service)
+    log('%s reloaded' % service, prefix='service')
---
+++ ploceus/tools/service.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from ploceus.helper import sudo
+
+
+def is_running(service):
+    # TODO: distro family
+    if 'running' in _service(service, 'status'):
+        return True
+    return False
+
+
+def start(service):
+    _service(service, 'start')
+
+
+def stop(service):
+    _service(service, 'stop')
+
+
+def restart(service):
+    _service(service, 'restart')
+
+
+def reload(service):
+    _service(service, 'reload')
+
+
+def _service(service, action):
+    rv = sudo('service %s %s' % (service, action),
+              quiet=True).stdout.read().strip()
+    return rv
