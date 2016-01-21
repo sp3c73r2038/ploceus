c08b2c553cc91d04f772f7d8ae7a9c39c680e538
nobody <nobody@nowhere.org>
2016-01-21 15:11:23 +0800

    add cli help

--- Ploceusfile.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from ploceus.api import task
-from ploceus.runtime import context_manager
-
-@task
-def test():
-    ctx = context_manager.get_context()
-    print(ctx['extra_vars'])
