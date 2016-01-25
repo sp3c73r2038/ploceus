16cb4f9f16f4b3d1ddad0f6f561474f58b46b4e7
80x24 <80x24@momoka.net>
2016-01-25 17:51:37 +0800

    context manager should not be static

--- ploceus/api.py
+++ ploceus/api.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
+from ploceus.context import cd, local_mode
 from ploceus.decorator import task
 from ploceus.helper import local, run, sudo
-from ploceus.runtime import cd, local_mode
 from ploceus.task import TaskRunner
 
 
--- ploceus/runtime.py
+++ ploceus/runtime.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-from contextlib import contextmanager
-import os
-
 from ploceus.context import ContextManager
 from ploceus.environment import Environment
 
@@ -9,30 +6,3 @@ from ploceus.environment import Environment
 
 context_manager = ContextManager()
 env = Environment()
-
-
-def cd(path):
-    path = path.replace(' ', '\ ')
-    if env.cwd and \
-       not path.startswith('/') and \
-       not path.startswith('~'):
-        new_cwd = os.path.join(env.cwd, path)
-    else:
-        new_cwd = path
-
-    return _setenv('cwd', new_cwd)
-
-
-def local_mode():
-    return _setenv('local_mode', True)
-
-
-@contextmanager
-def _setenv(name, value):
-    previous = getattr(env ,name)
-    setattr(env, name, value)
-    yield
-    if previous:
-        setattr(env, name, previous)
-        return
-    setattr(env, name, None)
