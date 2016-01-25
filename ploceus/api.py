c76b7a162ec6ea327bf4128a9eb2e28576a7a061
80x24 <80x24@momoka.net>
2016-01-25 11:36:59 +0800

    use TaskRunner

--- ploceus/api.py
+++ ploceus/api.py
@@ -2,9 +2,10 @@
 from ploceus.decorator import task
 from ploceus.helper import local, run, sudo
 from ploceus.runtime import cd
+from ploceus.task import TaskRunner
 
 
-def run_task_by_group(group_name, task, extra_vars=None):
+def run_task_by_group(group_name, tasks, extra_vars=None, parallel=False):
     from ploceus import g
     g.inventory.find_inventory()
     group = g.inventory.get_target_hosts(group_name)
@@ -12,5 +13,9 @@ def run_task_by_group(group_name, task, extra_vars=None):
     extra_vars = extra_vars or {}
     if 'vars' in group:
         extra_vars.update(group['vars'])
-    for host in hosts:
-        task.run(host, extra_vars)
+
+    if type(tasks) != list:
+        tasks = [tasks]
+
+    for task in tasks:
+        TaskRunner.run_task_with_hosts(task, hosts, parallel=parallel)
