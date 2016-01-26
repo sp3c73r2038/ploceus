b0c9dcff5355ba3f30d7b038d0bd50036baf8beb
80x24 <80x24@momoka.net>
2016-01-26 11:19:17 +0800

    add tools.system.cpus

--- ploceus/tools/system.py
+++ ploceus/tools/system.py
@@ -3,3 +3,9 @@ from ploceus.helper import run
 
 def time():
     return int(run('date +%s', quiet=True).stdout.strip())
+
+
+def cpus():
+    return int(run(("cat /proc/cpuinfo | "
+                    "grep -P 'processor\t: "
+                    "' | wc -l"), quiet=True).stdout.strip())
