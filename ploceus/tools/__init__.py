8ea1ee01a3c3015359e944f93c5dadddcc256f49
nobody <nobody@nowhere.org>
2016-01-27 13:22:01 +0800

    add tools.network, download and get_url

--- ploceus/tools/__init__.py
+++ ploceus/tools/__init__.py
@@ -2,6 +2,7 @@
 from ploceus.tools import deb
 from ploceus.tools import files
 from ploceus.tools import group
+from ploceus.tools import network
 from ploceus.tools import service
 from ploceus.tools import system
 from ploceus.tools import users
---
+++ ploceus/tools/declaration/network.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from ploceus.colors import blue
+from ploceus.logger import log
+from ploceus.tools import files
+from ploceus.tools import network
+
+
+
+def get_url(url, path, user=None, grp=None, mode=None,
+            sha1sum=None, use_sudo=False):
+
+    log('%s -> %s' % (url, path), prefix=blue('network'))
+
+    if sha1sum and files.is_file(path):
+        if sha1sum.lower() == files.sha1sum(path):
+            return
+
+    network.download(url, path, use_sudo=use_sudo)
+
+    if (user and user != files.owner(path)) or \
+       (grp and grp != files.group(path)):
+        files.chown(path, user=user, grp=grp, use_sudo=use_sudo)
+
+    if mode and mode != files.mode(path):
+        files.chmod(path, mode, use_sudo=use_sudo)
+
+    # TODO: verify checksum after download?
