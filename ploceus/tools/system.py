58517df47de159dca278563bf4b243b954f83c52
aleiphoenix <a@momoka.net>
2019-09-18 11:25:13 +0800

    add system.{release,codename,distro}

--- ploceus/tools/system.py
+++ ploceus/tools/system.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from ploceus.helper import run
 
+
 def time():
     return int(run('date +%s', quiet=True, silence=True).stdout.strip())
 
@@ -9,3 +10,24 @@ def cpus():
     return int(run(('cat /proc/cpuinfo | '
                     'grep -P "processor\t:"'
                     ' | wc -l'), quiet=True, silence=True).stdout.strip())
+
+
+def distro():
+    """
+    发行版的名称
+    """
+    return run('lsb_release -s -i', quiet=True, silence=True).stdout.strip()
+
+
+def codename():
+    """
+    发行版版本的代号
+    """
+    return run('lsb_release -s -c', quiet=True, silence=True).stdout.strip()
+
+
+def release():
+    """
+    发行版版本的发行版本
+    """
+    return run('lsb_release -s -r', quiet=True, silence=True).stdout.strip()
